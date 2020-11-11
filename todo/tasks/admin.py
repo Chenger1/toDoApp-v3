01033a6f24a98ef0e5b0af3d208d9e06ea053146
Vladislav <exs2199@gmail.com>
2020-11-11 18:04:02 +0200

    update admin.py: Add additional fields
    Add additional fields for task`s page

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -6,4 +6,9 @@ from .models import Task
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'status', 'created', 'updated']
-
+    list_filter = ['author', 'status', 'created']
+    search_fields = ('title', 'author__first_name')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'created'
+    raw_id_fields = ('author',)
+    ordering = ('status', 'created')

84358ed8331708b2eec3ba3eeacc0eff91307f80
Vladislav <exs2199@gmail.com>
2020-11-11 17:55:43 +0200

    feat: Create task model

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='created')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='saved_tasks')
+
+    description = models.TextField(max_length=250, blank=True)
+
+    status = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+

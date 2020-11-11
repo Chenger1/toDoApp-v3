2894bf230c8f50990ba8c1076346a7813337e42c
Vladislav <exs2199@gmail.com>
2020-11-11 18:20:41 +0200

    feat: Create test
    Create task for tasks model to check title label

--- todo/tasks/tests.py
+++ todo/tasks/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Task
+
+
+class TaskTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create(username='test')
+        Task.objects.create(title='second',
+                            author=User.objects.get(username='test'),
+                            )
+
+    def test_title_label(self):
+        task = Task.objects.get(id=1)
+        field_label = task._meta.get_field('title').verbose_name
+        self.assertEquals(field_label, 'title')

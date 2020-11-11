cd81db9d700a965d78a23bc57e7bf1174a366566
Vladislav <exs2199@gmail.com>
2020-11-11 18:50:19 +0200

    feat: Add Category model
    Now user can create and use different categories to dividing tasks into groups

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 
-from .models import Task
+from .models import Task, Category
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'status', 'created', 'updated']
-    list_filter = ['author', 'status', 'created']
+    list_filter = ['author', 'status', 'created', 'category__name']
     search_fields = ('title', 'author__first_name')
     prepopulated_fields = {'slug': ('title',)}
     date_hierarchy = 'created'
     raw_id_fields = ('author',)
     ordering = ('status', 'created')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created']
+    prepopulated_fields = {'slug': ('name',)}
--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150,
+                            unique_for_date='created')
+
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='categories')
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Task(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=250,
@@ -16,6 +31,10 @@ class Task(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    category = models.OneToOneField(Category,
+                                    related_name='categories',
+                                    on_delete=models.CASCADE,)
 
     class Meta:
         ordering = ['-created']

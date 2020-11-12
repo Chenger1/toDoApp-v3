6a058d6033b025d8d67438f2889b41cb62f442f5
Vladislav <exs2199@gmail.com>
2020-11-12 17:15:26 +0200

    feat: Create registration view

--- todo/users/urls.py
+++ todo/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from .views import UserRegistrationView
+
 
 app_name = 'users'
 
 urlpatterns = [
-
+    path('registration/', UserRegistrationView.as_view(), name='user_registration'),
 ]
--- todo/users/views.py
+++ todo/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+class UserRegistrationView(CreateView):
+    template_name = 'users/user/registration.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('tasks.task_list')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        cd = form.cleaned_data
+        user = authenticate(username=cd['username'],
+                            password=cd['password'])
+        login(self.request, user)
+        return result

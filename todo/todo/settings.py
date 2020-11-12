a461f6788bbbb9babf741fc3b9b17faf51ee77b1
Vladislav <exs2199@gmail.com>
2020-11-12 17:09:44 +0200

    structure: Create new app "Users"
    New will provide view for handling registration and processing user`s data

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks.apps.TasksConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls', namespace='tasks'))
+    path('tasks/', include('tasks.urls', namespace='tasks')),
+    path('users/', include('users.urls', namespace='users')),
 ]

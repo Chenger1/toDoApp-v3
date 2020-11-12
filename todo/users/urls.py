78a7b3b909bc99db10e72fe5f11ba94bc2149d1c
Vladislav <exs2199@gmail.com>
2020-11-12 18:53:53 +0200

    feat and update: Create login.html
    Update urls to work with auth correct

--- todo/users/urls.py
+++ todo/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import UserRegistrationView
-
 
 app_name = 'users'
 
 urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('registration/', UserRegistrationView.as_view(), name='user_registration'),
 ]

fb677681503d9c201146a5c3226e3662c8e3c1c8
Vladislav <exs2199@gmail.com>
2020-11-12 16:30:16 +0200

    feat: Add task view
    Create view to render list of all tasks

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+from .models import Task
+
+
+class TaskList(ListView):
+    model = Task
+    template_name = 'task/list.html'

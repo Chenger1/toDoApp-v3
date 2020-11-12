0fb30b088d469d94e3f336c98938b5907adf6f58
Vladislav <exs2199@gmail.com>
2020-11-12 17:24:41 +0200

    feat: Create registration template and fix bug
    Also, make a little fix with reverse_lazy func. Fixed wrong url name

--- todo/users/views.py
+++ todo/users/views.py
@@ -7,12 +7,12 @@ from django.contrib.auth import authenticate, login
 class UserRegistrationView(CreateView):
     template_name = 'users/user/registration.html'
     form_class = UserCreationForm
-    success_url = reverse_lazy('tasks.task_list')
+    success_url = reverse_lazy('tasks:task_list')
 
     def form_valid(self, form):
         result = super().form_valid(form)
         cd = form.cleaned_data
         user = authenticate(username=cd['username'],
-                            password=cd['password'])
+                            password=cd['password1'])
         login(self.request, user)
         return result

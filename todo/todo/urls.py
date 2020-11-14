7176d466951a6dc48ca6a1e237cfb6497ed6183f
Vladislav <exs2199@gmail.com>
2020-11-14 18:27:24 +0200

    update: Change main url to show tasks page

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls', namespace='tasks')),
+    path('', include('tasks.urls', namespace='tasks')),
     path('users/', include('users.urls', namespace='users')),
 ]

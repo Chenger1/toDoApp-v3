55bf79cf67e99af14553d2fe80f5bbf293718b95
Vladislav <exs2199@gmail.com>
2020-11-17 19:29:33 +0200

    feat: Add profile

--- todo/users/urls.py
+++ todo/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import UserRegistrationView
+from .views import UserRegistrationView, UserDetailView
 
 app_name = 'users'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('registration/', UserRegistrationView.as_view(), name='user_registration'),
+    path('detail/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
 ]
--- todo/users/views.py
+++ todo/users/views.py
@@ -1,7 +1,12 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+from tasks.models import Task, Category
 
 
 class UserRegistrationView(CreateView):
@@ -15,4 +20,26 @@ class UserRegistrationView(CreateView):
         user = authenticate(username=cd['username'],
                             password=cd['password1'])
         login(self.request, user)
+        return result
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'users/user/detail.html'
+    user = None
+
+    def get(self, request, *args, **kwargs):
+        self.user = get_object_or_404(User, pk=kwargs['pk'])
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        result = super().get_context_data(**kwargs)
+        task_queryset = Task.objects.filter(author=self.user)
+        task_counter = task_queryset.count()
+        category_queryset = Category.objects.filter(author=self.user)
+
+        result['tasks'] = task_queryset
+        result['counter'] = task_counter
+        result['categories'] = category_queryset
+
         return result
